48c52708d1f97e8305588573aaf2eec61b6d2af3
Prasadaketi <104889674+Prasadaketi@users.noreply.github.com>
2023-09-24 19:23:55 +0530

    strings-conversions.py

--- main.py
+++ main.py
@@ -16,3 +16,42 @@ message : str = "Hi, How are you?"
 print(message)
 print(type(message))
 
+#binary
+a = str(0b0110)
+print(a)
+print(type(a))
+
+#octal
+a = str(0o3214)
+print(a)
+print(type(a))
+
+#hexa
+a = str(0x34bd)
+print(a)
+print(type(a))
+
+#boolean
+a = str(False)
+print(a)
+print(type(a))
+
+#binary conversion
+b = bin("prasad")
+print(a)
+print(type(a))
+
+#octal conversion
+b = oct("hello")
+print(a)
+print(type(a))
+
+#hexa conversion
+c = hex("hi")
+print(a)
+print(type(a))
+
+#boolean conversion
+d = bool("abc")
+print(a)
+print(type(a))
