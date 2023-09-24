277b8de99857aea4e0fa21bffb4b0fe995414df6
Prasadaketi <104889674+Prasadaketi@users.noreply.github.com>
2023-09-24 19:01:34 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+# Implicit string datatype
+
+name = "prasad"
+print(name)
+print(type(name))
+
+#explicit string datatype
+
+message = str("Hello")
+print(message)
+print(type(message))
+
+#variable/ datatype annotation
+
+message : str = "Hi, How are you?"
+print(message)
+print(type(message))
+
