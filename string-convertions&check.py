b5e939aae8e02ccff5ef06a8d1fced4a9912ff42
Prasadaketi <104889674+Prasadaketi@users.noreply.github.com>
2023-09-26 18:06:55 +0530

    Add files via upload

---
+++ string-convertions&check.py
@@ -0,0 +1,70 @@
+#string conversion methods
+#capitalize
+name = "hello how are you?"
+print(name.capitalize())
+
+#title
+name = "prasad aketi"
+print(name.title())
+
+#upper
+name = "prasad"
+print(name.upper())
+
+#lower
+name = "MY NAME IS PRASAD"
+print(name.lower())
+
+#casefold
+name = "PRASAD AKETI"
+print(name.casefold())
+
+#swapcase
+name = "PRASAD aketi"
+print(name.swapcase())
+
+#string check functions
+#isnumeric
+a = "12234"
+print(a.isnumeric())
+
+#isalnum
+a = "prasad25"
+print(a.isalnum())
+b = "prasad"
+print(b.isalnum())
+
+#isdigit
+a = "235"
+print(a.isdigit())
+
+#isasscii
+a = "G4G"
+print(a.isascii())
+
+#isupper
+a = "PRASAD AKETI"
+print(a.isupper())
+
+#islower
+a = "prasadaketi"
+print(a.islower())
+
+#isspace
+a = " "
+print(a.isspace())
+
+b = "prasad"
+print(b.isspace())
+
+#isidentifier
+a = "prasad_aketi25"
+print(a.isidentifier())
+
+#isprintable
+a = "prasad_aketi6342"
+print(a.isprintable())
+
+#istitle
+a = "Hello, Welcome!"
+print(a.istitle())
